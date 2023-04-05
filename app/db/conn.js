14629d40546245681ea0e0e08000fec4ce412130
dento65 <zaiddento@gmail.com>
2023-04-05 21:03:11 +0530

    1

--- app/db/conn.js
+++ app/db/conn.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 // const DB = process.env.DATABASE
-const DB = "mongodb://127.0.0.1:27017/mydatabase";
+// const DB = "mongodb://127.0.0.1:27017/mydatabase";
+const DB =
+  "mongodb+srv://zaiddento:mi3HF-8RGj5J4Kz@cluster0.3tqgcv3.mongodb.net/?retryWrites=true&w=majority";
 console.log(process.env.port);
 mongoose
   .connect(process.env.MONGODB_URI || DB, {
